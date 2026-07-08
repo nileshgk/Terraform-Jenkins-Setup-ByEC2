687f762b64e0b0394f3f1a5fc42f2e71f4e412cf
nileshgk <nileshgk774@gmail.com>
2026-07-08 21:05:54 +0530

    updated ami id

--- Infra-Jenkins/variable.tf
+++ Infra-Jenkins/variable.tf
@@ -1,7 +1,7 @@
 variable "ami_id" {
     description = "AMI ID for the Jenkins EC2 instance"
     type        = string
-    default     = "ami-08f44e8eca9095668" # Example AMI ID, replace with a valid one 
+    default     = "ami-002192a70217ac181" # Example AMI ID, replace with a valid one 
 }
 
 variable "instance_type" {
